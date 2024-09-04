0ae3a4c383a9d99438304721987950cd164b7630
EpicIbby101 <kevin_grittner@outlook.com>
2024-09-04 18:40:15 +0100

    First commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,160 +1,132 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import type { JSX } from "react";
-import { useSearchParams } from "next/navigation";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import ButtonSignin from "./ButtonSignin";
-import logo from "@/app/icon.png";
-import config from "@/config";
+import Logo from "@/public/logo.png";
+import github from "@/public/github-logo.png";
+import linkedin from "@/public/linkedin.png";
+import { useSearchParams } from "next/navigation";
 
-const links: {
-  href: string;
-  label: string;
-}[] = [
+const links = [
   {
-    href: "/#pricing",
-    label: "Pricing",
+    href: "/nintendo",
+    label: "Nintendo",
   },
   {
-    href: "/#testimonials",
-    label: "Reviews",
+    href: "/sega",
+    label: "Sega",
   },
   {
-    href: "/#faq",
-    label: "FAQ",
+    href: "/playstation",
+    label: "Playstation",
   },
 ];
 
-const cta: JSX.Element = <ButtonSignin extraStyle="btn-primary" />;
-
-// A header with a logo on the left, links in the center (like Pricing, etc...), and a CTA (like Get Started or Login) on the right.
-// The header is responsive, and on mobile, the links are hidden behind a burger button.
 const Header = () => {
   const searchParams = useSearchParams();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // setIsOpen(false) when the route changes (i.e: when the user clicks on a link on mobile)
   useEffect(() => {
-    setIsOpen(false);
+    setIsMenuOpen(false);
   }, [searchParams]);
 
   return (
-    <header className="bg-base-200">
+    <header className="bg-emerald-600 top-0 w-full z-50">
       <nav
-        className="container flex items-center justify-between px-8 py-4 mx-auto"
+        className="container flex items-center justify-between px-5 sm:px-0 md:px-0 lg:px-4 py-3 mx-auto"
         aria-label="Global"
       >
-        {/* Your logo/name on large screens */}
         <div className="flex lg:flex-1">
           <Link
-            className="flex items-center gap-2 shrink-0 "
+            className="flex items-center gap-2 shrink-0"
             href="/"
-            title={`${config.appName} homepage`}
+            title="Homepage"
           >
             <Image
-              src={logo}
-              alt={`${config.appName} logo`}
-              className="w-8"
+              src={Logo}
+              alt="Logo"
+              className="w-10"
               placeholder="blur"
               priority={true}
-              width={32}
-              height={32}
             />
-            <span className="font-extrabold text-lg">{config.appName}</span>
+
+            <span className="font-semibold text-xl py- pr-5">
+              My Childhood Games
+            </span>
           </Link>
         </div>
-        {/* Burger button to open menu on mobile */}
-        <div className="flex lg:hidden">
+
+        <div className="flex lg:hidden ml-auto">
+          <button
+            className="lg:hidden"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div className="flex items-center lg:justify-end lg:flex-1">
+          <div className="hidden lg:flex lg:justify-center lg:gap-3 lg:items-center">
+            {links.map((link, index) => (
+              <React.Fragment key={link.href}>
+                <Link
+                  href={link.href}
+                  key={link.href}
+                  className="link link-hover block py-2 md:py-0 md:inline-block hover:text-gray-500 duration-300"
+                  title={link.label}
+                >
+                  {link.label}
+                </Link>
+                {index <= links.length - 1 && (
+                  <span className="hidden md:inline-block border-l border-gray-300 h-4 mx-3"></span>
+                )}
+              </React.Fragment>
+            ))}
+            
+          </div>
+          
+        </div>
+      </nav>
+
+      <div className={`relative z-50 ${isMenuOpen ? "" : "hidden"} lg:hidden`}>
+        <div
+          className={`fixed inset-y-0 right-0 z-10 w-full px-8 py-4 overflow-y-auto bg-emerald-600 sm:max-w-sm sm:ring-1 sm:ring-neutral/10 transform origin-right transition ease-in-out duration-300`}
+        >
           <button
             type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
-            onClick={() => setIsOpen(true)}
+            className="-m-2.5 rounded-md p-2.5"
+            onClick={() => setIsMenuOpen(false)}
           >
-            <span className="sr-only">Open main menu</span>
+            <span className="sr-only">Close menu</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-6 h-6 text-base-content"
+              className="w-7 h-7"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                d="M6 18L18 6M6 6l12 12"
               />
             </svg>
           </button>
-        </div>
-
-        {/* Your links on large screens */}
-        <div className="hidden lg:flex lg:justify-center lg:gap-12 lg:items-center">
-          {links.map((link) => (
-            <Link
-              href={link.href}
-              key={link.href}
-              className="link link-hover"
-              title={link.label}
-            >
-              {link.label}
-            </Link>
-          ))}
-        </div>
-
-        {/* CTA on large screens */}
-        <div className="hidden lg:flex lg:justify-end lg:flex-1">{cta}</div>
-      </nav>
-
-      {/* Mobile menu, show/hide based on menu state. */}
-      <div className={`relative z-50 ${isOpen ? "" : "hidden"}`}>
-        <div
-          className={`fixed inset-y-0 right-0 z-10 w-full px-8 py-4 overflow-y-auto bg-base-200 sm:max-w-sm sm:ring-1 sm:ring-neutral/10 transform origin-right transition ease-in-out duration-300`}
-        >
-          {/* Your logo/name on small screens */}
-          <div className="flex items-center justify-between">
-            <Link
-              className="flex items-center gap-2 shrink-0 "
-              title={`${config.appName} homepage`}
-              href="/"
-            >
-              <Image
-                src={logo}
-                alt={`${config.appName} logo`}
-                className="w-8"
-                placeholder="blur"
-                priority={true}
-                width={32}
-                height={32}
-              />
-              <span className="font-extrabold text-lg">{config.appName}</span>
-            </Link>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5"
-              onClick={() => setIsOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-
-          {/* Your links on small screens */}
           <div className="flow-root mt-6">
             <div className="py-4">
               <div className="flex flex-col gap-y-4 items-start">
@@ -162,7 +134,7 @@ const Header = () => {
                   <Link
                     href={link.href}
                     key={link.href}
-                    className="link link-hover"
+                    className="link link-hover hover:text-gray-600 hover:underline"
                     title={link.label}
                   >
                     {link.label}
@@ -170,9 +142,33 @@ const Header = () => {
                 ))}
               </div>
             </div>
-            <div className="divider"></div>
-            {/* Your CTA on small screens */}
-            <div className="flex flex-col">{cta}</div>
+          </div>
+          <hr className="my-4 border-t border-gray-300" />{" "}
+          <div className="flex flex-row space-x-3">
+            <Link
+              className="flex items-center gap-2 shrink-0 hover:scale-110 transition-all duration-200"
+              href="https://www.linkedin.com/in/kevin-grittner-770465243/"
+            >
+              <Image
+                src={linkedin}
+                alt="linkedin"
+                className="w-10"
+                placeholder="blur"
+                priority={true}
+              />
+            </Link>
+            <Link
+              className="flex items-center gap-2 shrink-0 hover:scale-110 transition-all duration-200"
+              href="https://github.com/EpicIbby101"
+            >
+              <Image
+                src={github}
+                alt="github"
+                className="w-10"
+                placeholder="blur"
+                priority={true}
+              />
+            </Link>
           </div>
         </div>
       </div>
